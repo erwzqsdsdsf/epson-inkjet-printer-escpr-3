4829b6ae40b8bc070c321af83c262c9448f751e2
Didier Raboud <odyx@debian.org>
2013-06-16 09:12:48 +0200

    Imported Upstream version 1.2.3

--- src/debug.h
+++ src/debug.h
@@ -1,7 +1,7 @@
 /*
  * Epson Inkjet Printer Driver (ESC/P-R) for Linux
  * Copyright (C) 2002-2005 AVASYS CORPORATION.
- * Copyright (C) Seiko Epson Corporation 2002-2012.
+ * Copyright (C) Seiko Epson Corporation 2002-2013.
  *
  *  This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
